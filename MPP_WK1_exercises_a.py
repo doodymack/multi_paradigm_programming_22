fe818bbf824e678cb88d7c5d1b14dbbbb589e4d3
Paul Mc Grath <G00398606@gmit.ie>
2022-10-03 19:59:36 +0100

    coursework_wk_2

---
+++ MPP_WK1_exercises_a.py
@@ -0,0 +1,7 @@
+#MPP_WK1_exercises_a
+
+#https://www.codingem.com/python-how-to-ask-for-user-input/
+
+# #the split() method splits the input based on a blank space by default
+fname, lname = input("Enter your full name: ").split()
+print(f"Hello {fname} {lname}")
